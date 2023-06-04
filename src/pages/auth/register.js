93a054f555aea18642c350786dcf6a5abdd2b2bb
Muhammad Khoirul Huda <79193720+khoirulhudaa@users.noreply.github.com>
2023-06-04 22:16:05 +0700

    fix

--- src/pages/auth/register.js
+++ src/pages/auth/register.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react'
 import {Button, Input} from '../../component/'
 import bgLine from '../../assets/images/svg/bgLine.svg'
 import { useNavigate } from 'react-router-dom'
+import Human1 from '../../assets/images/svg/human1.png'
+import Human2 from '../../assets/images/svg/human2.png'
+import Human3 from '../../assets/images/svg/human3.png'
+import Google from '../../assets/images/png/google.png'
 
 const Login = () => {
 
@@ -10,10 +14,8 @@ const Login = () => {
   
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
-  const [textError, setTextError] = useState("SIGN UP")
+  const [textError, setTextError] = useState("SIGN IN")
 
-  const [isError, setIsError] = useState(false)
-  
   const handleSubmit = async(e) => {
     e.preventDefault()
 
@@ -42,43 +44,39 @@ const Login = () => {
   }
 
   return (
-    <div className='w-screen h-[100vh] flex items-center justify-center overflow-hidden'>
-      <div className='bg-indigo-500 absolute ml-auto mr-auto flex w-full h-full z-[-1]'>
-        <img src={bgLine} alt="bg" className='w-full h-full absolute ml-auto mr-auto flex' />
+    <div className='relative w-screen bg-darkMongo h-[100vh] flex items-center justify-center overflow-hidden'>
+      <div className='w-[30%] h-screen bg-[#00684A] p-5 overflow-hidden rounded-tr-[80px] shadow-lg'>
+        <h2 className='text-white text-[40px] mb-[30px]'>Create your account</h2>
+        <div className='w-[90%] h-[1px] bg-white my-1'></div>
+        <div className='border-[1px] px-4 py-1 mt-8 border-white text-white w-[90%] rounded-lg cursor-pointer hover:shadow-saweria active:scale-[0.97] flex items-center justify-center'>
+          <img src={Google} className='w-[34px] h-[34px] mr-3' alt='icon' />
+          <p className='relative top-2'>Google</p>
+        </div>
+        <div className='w-full mb-4 mt-4'>
+            <label htmlFor="username" className='mb-1 text-white font-normal'>Username</label>
+            <input type="text" placeholder='Enter username' name='username' className='font-normal text-[14px] outline-0 rounded-lg py-[10px] px-3 w-[90%]' />
+        </div>
+        <div className='w-full mb-4 mt-4'>
+            <label htmlFor="email" className='mb-1 text-white font-normal'>Email</label>
+            <input type="text" name='email' placeholder='Enter email' className='font-normal text-[14px] outline-0 rounded-lg py-[10px] px-3 w-[90%]' />
+        </div>
+        <div className='w-full mb-4 mt-4'>
+            <label htmlFor="password" className='mb-1 text-white font-normal'>Password</label>
+            <input type="password" name='password' placeholder='Enter password' className='font-normal text-[14px] outline-0 rounded-lg py-[10px] px-3 w-[90%]' />
+        </div>
+        <div className='rounded-lg border-[1px] border-white text-center py-2 w-max px-3 cursor-pointer active:scale-[0.97] text-white'>
+          Enter now
+        </div>
       </div>
-      <div className='w-[80%] relative h-[100vh] flex items-center justify-center ml-auto'>
-        <div className={`userSI text-[20px] ${textError == 'SIGN UP' ? "bg-indigo-500" : ""} absolute left-[200px] top-[160px] font-normal w-max h-max border rotate-[25deg] border-white rounded-sm text-center text-white px-6 py-3`}>
-          SIGN UP
-        </div>
-        <div className={`userWP text-[20px] ${textError == 'WERONG PAZZWOT!' ? "bg-red-400 text-white" : ""} absolute top-[200px] left-[-200px] font-normal w-max h-max text-white border rotate-[-40deg] border-white rounded-sm px-6 py-3`}>
-          WERONG PAZZWOT!
-        </div>
-        <div className={`userNF text-[20px] ${textError == 'USER NOT-FON' ? "bg-red-400" : ""} absolute left-[180px] font-normal w-max h-max bottom-[170px] text-white border rotate-[-50deg] border-white rounded-sm text-center px-6 py-3`}>
-          USER NOT-FON
-        </div>
-        <div className='userTME text-[16px] absolute left-[-100px] font-normal w-max h-max bottom-[170px] border rotate-[-50deg] border-white rounded-sm text-center text-white px-6 py-3'>
-          TU MENY ETEMPS
-        </div>
-        <div className='w-[44%] h-[70vh] bg-white flex rounded-lg ml-auto flex mr-[150px] shadow-lg flex items-center'>
-          <div className='w-[44%] border border-black border-[1px] h-[100%] p-2 flex flex-col items-center justify-center text-center'>
-              <p className='textStroke text-stroke relative text-[50px]'>1440</p>
-              <p className='textStroke text-stroke relative top-[-12px] text-[50px]'>1440</p>
-              <p className='textStroke text-stroke relative top-[-12px] text-[50px]'>1440</p>
-              <p className='textStroke text-stroke relative top-[-12px] text-[50px]'>1440</p>
-              <Button onClick={(e) => handleSubmit(e)} text="Sign in" padding={'px-[28px] py-2'} />
-          </div>
-          <div className='w-[50%] h-[100%] p-2 overflow-hidden'>
-              <div className='w-full p-2 mb-4 mt-4'>
-                  <label htmlFor="username" className='mb-1 font-normal'>Username</label>
-                  <Input id='username' name="username" placeholder="Masukan password" value={username} onChange={(e) => setUsername(e.target.value)} />
-              </div>
-              <div className='w-[100%] w-full p-2'>
-                  <label htmlFor="password" className='mb-1 font-normal'>Password</label>
-                  <Input id='password' name="password" type="password" placeholder={'Masukan password'} value={password} onChange={(e) => setPassword(e.target.value)} />
-              </div>
-              <small className='text-[12px] font-normal left-4 top-4 relative'>Already have an account? <span className='text-blue-500 cursor-pointer hover:text-blue-400' onClick={() => navigate('/signIn')}>signin</span></small>
-          </div>
-        </div>
+      <div className='w-[70%] relative h-[100vh] flex items-center justify-center ml-auto overflow-hidden'>
+        <img src={Human1} className='w-[300px] bottom-0 left-10 absolute' alt="human" />
+        <img src={Human2} className='w-[260px] absolute top-0 left-[340px] rotate-[180deg]' alt="human" />
+        <img src={Human3} className='w-[300px] absolute bottom-0 right-[0px]' alt="human" />
+        <a href="/signIn">
+          <span className='absolute top-10 text-white text-[16px] right-10'>
+            Have an account? <span className='text-blue-500 cursor-pointer'>Log in now  </span>
+          </span>
+        </a>
       </div>
     </div>
   )
