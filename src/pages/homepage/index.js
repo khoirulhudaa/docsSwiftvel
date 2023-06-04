316a94babf4e41bb6cfb435e864f9c8da0b06be5
Muhammad Khoirul Huda <79193720+khoirulhudaa@users.noreply.github.com>
2023-06-04 14:28:11 +0700

    fix

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -66,7 +66,7 @@ const Homepage = () => {
       {/* Back to top */}
       {
         isVisible ? (
-          <div onClick={scrollToTop} className={`duration-100 fixed z-40 w-[50px] h-[50px] lg:w-[60px] lg:h-[60px] right-[100px] lg:right-[50px] bottom-[50px] rounded-full bg-mongo text-white flex items-center justify-center cursor-pointer shadow-lg`}>
+          <div onClick={scrollToTop} className={`duration-100 fixed z-40 w-[45px] h-[45px] lg:w-[60px] lg:h-[60px] right-[30px] lg:right-[50px] bottom-[70px] lg:bottom-[50px] rounded-full bg-mongo text-white flex items-center justify-center cursor-pointer shadow-lg`}>
               <img src={Chevron} className='chevronAnim w-[18px]' alt="icon" />
           </div>
         ) : 
