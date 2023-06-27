d73e5fe0fc34e2e81145340c39ccae1098c2ef34
valclassdevelop <developervalclass.com>
2023-06-27 15:55:34 +0700

    fix

--- src/pages/contributors/index.js
+++ src/pages/contributors/index.js
@@ -24,7 +24,7 @@ const Contributors = () => {
       }
         <section className='w-full 2xl:h-[80vh] h-[93vh] bg-bgMongo lg:pb-[0px] pb-4 pt-[50px] lg:pt-[80px] text-center'>
             <h1 className='text-[43px] lg:text-[80px] 2xl:text-[100px] 2xl:mt-10 w-[70%] text-white ml-auto mr-auto font-normal leading-[1.5em] mb-4'>Those who <span className='text-lightMongo'>contributed</span> to dragme</h1>
-            <div className='lg:flex justify-center mt-[50px] 2xl:mt-[100px] lg:mt-[80px] w-screnn items-center'>
+            <div className='lg:flex w-screen justify-center mt-[50px] 2xl:mt-[100px] lg:mt-[80px] w-screnn items-center'>
                 <a href="https://github.com/imzzan" className='no-underline text-black' target='__blank'>
                     <div className='flex ml-auto mr-auto lg:ml-0 lg:mr-0 hover:brightness-[90%] hover:scale-[0.98] active:scale-[0.96] cursor-pointer items-center overflow-hidden justify-between w-[70vw] lg:w-max px-6 py-1 h-max bg-white rounded-full'>
                         <a href="https://github.com/imzzan" target='__blank'>
@@ -32,7 +32,7 @@ const Contributors = () => {
                                 <img src={Github} alt="img" className='w-full h-auto cursor-pointer hover:brightness-[90%] active:scale-[0.98]' />
                             </div>
                         </a>                           
-                        <p className='relative mx-4 top-2 font-normal'>imzzan</p>
+                        <p className='relative mx-4 top-0 font-normal'>imzzan</p>
                         <div className='w-[30px] h-[30px] rounded-full overflow-hidden'>
                             <img src={G1} alt="img" className='w-full h-auto cursor-pointer hover:brightness-[90%] active:scale-[0.98]' />
                         </div>
@@ -45,7 +45,7 @@ const Contributors = () => {
                                 <img src={Github} alt="img" className='w-full h-auto cursor-pointer hover:brightness-[90%] active:scale-[0.98]' />
                             </div>
                         </a>
-                        <p className='relative mx-4 top-2 font-normal'>justnotiyann</p>
+                        <p className='relative mx-4 top-0 font-normal'>justnotiyann</p>
                         <div className='w-[30px] h-[30px] rounded-full overflow-hidden'>
                             <img src={G1} alt="img" className='w-full h-auto cursor-pointer hover:brightness-[90%] active:scale-[0.98]' />
                         </div>
@@ -58,7 +58,7 @@ const Contributors = () => {
                                 <img src={Github} alt="img" className='w-full h-auto cursor-pointer hover:brightness-[90%] active:scale-[0.98]' />
                             </div>
                         </a>
-                        <p className='relative mx-4 top-2 font-normal'>khoirulhudaa</p>
+                        <p className='relative mx-4 top-0 font-normal'>khoirulhudaa</p>
                         <div className='w-[30px] h-[30px] rounded-full overflow-hidden'>
                             <img src={G2} alt="img" className='w-full h-auto cursor-pointer hover:brightness-[90%] active:scale-[0.98]' />
                         </div>
